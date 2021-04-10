1ac1e12ba4f32a41348317f90176e1fadd03b486
Camilo-HG <22363097+Camilo-HG@users.noreply.github.com>
2021-04-10 15:12:39 -0500

    Adding requirements.txt

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import uvicorn
 from src.config import settings
 
 if __name__ == '__main__':
+
     uvicorn.run(
         "src.api:app",
         host=settings['HOST'],
         port=int(settings['PORT']),
         reload=True,
-        debug=True, workers=1
-    )
+        debug=True,
+        workers=1
+        )
