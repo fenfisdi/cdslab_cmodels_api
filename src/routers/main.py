c6243d6fbbd9a10993f4afd14717895b8c09876f
Juan Esteban <jearistiz@Juans-MacBook-Pro.local>
2021-04-11 15:31:06 -0500

    feat: added main router (hello world) with appropiate unit test

---
+++ src/routers/main.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter, Request
+
+main_router_prefix = ""
+main_router = APIRouter(prefix=main_router_prefix)
+
+@main_router.get('/')
+async def hello(request: Request):
+    return {'hello': 'world'}
