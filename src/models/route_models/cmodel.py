f495634fdb751189f94fbdabd1f2e38fd58352b3
Juan Esteban Aristizabal <jeaz.git@gmail.com>
2021-04-14 12:58:58 -0500

    refactor: compartmental models' database models

--- src/models/route_models/cmodel.py
+++ src/models/route_models/cmodel.py
@@ -1,73 +1,78 @@
 from enum import Enum
-from typing import List
+from typing import Dict, List
 
 from pydantic import BaseModel
+
+
+class CompartmentalModelBase(BaseModel):
+    """Base Model for Compartmental Models
+    """
+    """Name of the compartmental model"""
+    name: str
+    """Name of state variables of corresponding model"""
+    state_variables: List[str]
+    """Units of each state variable. The keys are the 'state_variables' array elements"""
+    state_variables_units: Dict[str, str]
+    """Parameters of the corresponding model"""
+    parameters: List[str]
+    """Units of each parameter. The keys are the 'parameters' array elements"""
+    parameters_units: Dict[str, str]
 
 
 class CompartmentalModel(Enum):
     """Compartmental Models' Data.
 
-    Each element of this ``Enum`` object contains all the essential information on
-    the corresponding Compartmental Model. Each element is a ``dict``
-    conforming to the following model:
-
-    ```python
-    {
-        'name': str  # Name of the compartmental model
-        'state_variables': List[str]  # Name of state variables of corresponding model
-        'state_variables_units': Dict[str, str]  # Units of each state variable. The keys are the 'state_variables' array elements
-        'parameters': List[str]  # Parameters of the corresponding model
-        'parameters_units': Dict[str, str]  # Units of each parameter. The keys are the 'parameters' array elements
-    }
-    ```
+    Each element of this ``Enum`` class contains all the essential information
+    on the corresponding Compartmental Model. Each element is a
+    ``CompartmentalModelBase`` object
     """
-    sir = {
-        'name': 'SIR',
-        'state_variables': ['S', 'I', 'R'],
-        'state_variables_units': {
+    sir = CompartmentalModelBase(
+        name='SIR',
+        state_variables=['S', 'I', 'R'],
+        state_variables_units={
             'S': 'persons',
             'I': 'persons',
             'R': 'persons',
         },
-        'parameters': ['a', 'b'],
-        'parameters_units': {
+        parameters=['a', 'b'],
+        parameters_units={
             'a': 'units of a',
             'b': 'units of b',
         },
-    }
+    )
 
-    seir = {
-        'name': 'SEIR',
-        'state_variables': ['S', 'E', 'I', 'R'],
-        'state_variables_units': {
+    seir = CompartmentalModelBase(
+        name='SEIR',
+        state_variables=['S', 'E', 'I', 'R'],
+        state_variables_units={
             'S': 'persons',
             'E': 'persons',
             'I': 'persons',
             'R': 'persons',
         },
-        'parameters': ['a', 'b'],
-        'parameters_units': {
+        parameters=['a', 'b'],
+        parameters_units={
             'a': 'units of a',
             'b': 'units of b',
         },
-    }
+    )
 
-    seirv = {
-        'name': 'SEIRV',
-        'state_variables': ['S', 'E', 'I', 'R', 'V'],
-        'state_variables_units': {
+    seirv = CompartmentalModelBase(
+        name='SEIRV',
+        state_variables=['S', 'E', 'I', 'R', 'V'],
+        state_variables_units={
             'S': 'persons',
             'E': 'persons',
             'I': 'persons',
             'R': 'persons',
             'V': 'persons',
         },
-        'parameters': ['a', 'b'],
-        'parameters_units': {
+        parameters=['a', 'b'],
+        parameters_units={
             'a': 'units of a',
             'b': 'units of b',
         },
-    }
+    )
 
 
 class CModel(BaseModel):
