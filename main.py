ec83c827a4dcafc88a449af326657dd19961f506
Juan Esteban Aristizabal <jeaz.git@gmail.com>
2021-04-22 09:43:07 -0500

    refactor: changed back cmodels documents creation to main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import uvicorn
 
 from src.config import settings
+from src.use_cases.cmodels import CmodelUseCases
 
 
 if __name__ == '__main__':
+    CmodelUseCases.update_cmodels_collection()
     uvicorn.run(
         "src.api:app",
         host=settings['HOST'],
