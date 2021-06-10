6a6b4cceff9c489801bd372abf9fdaa1e5f1bc39
David Peña <dpenah@intergrupo.com>
2021-06-10 09:19:15 -0500

    Feature: Update Simulation Errors

--- src/use_cases/simulation.py
+++ src/use_cases/simulation.py
@@ -9,16 +9,22 @@ from dinjo.predefined.epidemiology import ModelSEIR, ModelSEIRV, ModelSIR
 from pandas import DataFrame
 from plotly.graph_objs import Figure
 
-from src.models.db import Simulation
-from src.models.general import ParameterType
+from src.models.db import Simulation, User
+from src.models.general import ParameterType, SimulationStatus
 from src.services import FileAPI
-from src.use_cases.graph import GraphUseCase
 from src.utils.date_time import DateTime
+from .email import SendEmailUseCase
+from .graph import GraphUseCase
 
 
 class ExecuteSimulationUseCase:
     @classmethod
-    def handle(cls, simulation: Simulation):
+    def handle(cls, simulation: Simulation, user: User):
+        try:
+            simulation.update(status=SimulationStatus.RUNNING)
+        finally:
+            simulation.reload()
+
         simulation_models = {
             'SIR': ModelSIR,
             'SEIR': ModelSEIR,
@@ -32,95 +38,110 @@ class ExecuteSimulationUseCase:
         )
         range_days = [0, days - 1]
 
-        model_template = simulation_models.get(simulation.model.name, None)
-        if not model_template:
-            raise RuntimeError("Model simulation didn't exist")
+        try:
+            model_template = simulation_models.get(simulation.model.name, None)
+            if not model_template:
+                raise RuntimeError("Model simulation didn't exist")
 
-        parameters = cls.get_parameters(simulation)
-        state_variables = cls.get_variable_state(simulation)
+            parameters = cls.get_parameters(simulation)
+            state_variables = cls.get_variable_state(simulation)
 
-        model = model_template(
-            state_variables=state_variables,
-            parameters=parameters,
-            t_span=range_days,
-            t_steps=days
-        )
-
-        start_time = time.time()
-        results = model.run_model(method='RK45')
-        df_results = cls.result_to_dataframe(
-            results,
-            simulation,
-            state_variables
-        )
-        is_optimized = simulation.parameter_type == ParameterType.OPTIMIZED
-
-        if is_optimized:
-
-            variable_to_fit = next((
-                variable for variable in simulation.state_variable_limits
-                if variable.to_fit
-            ), None)
-            assert variable_to_fit, "Undefined"
-
-            variable_model = next((
-                variable for variable in state_variables
-                if variable.name == variable_to_fit.representation
-            ), None)
-
-            file = cls.get_upload_file(simulation)
-            df_reference = cls.format_reference_dataframe(file, variable_model)
-            reference_values = df_reference[variable_to_fit.label].to_numpy()
-
-            # TODO: Verify Args for Optimization object
-            algorithm_kwargs = {
-                'popsize': 20,
-                'disp': True,
-                'tol': 0.0015,
-                'maxiter': 100,
-                'mutation': [0.3, 0.7],
-                'atol': 200
-            }
-
-            optimizer = Optimizer(
-                model=model,
-                reference_state_variable=variable_model,
-                reference_values=reference_values,
-                reference_t_values=results.t
-            )
-
-            optimal = optimizer.optimize(
-                cost_method='root_mean_square',
-                algorithm='differential_evolution',
-                algorithm_kwargs=algorithm_kwargs
-            )
-            optimized_params = optimal.x
-
-            # Update Params
-            init_params = parameters.copy()
-            for index in range(len(parameters)):
-                init_params[index].initial_value = optimized_params[index]
-
-            # Update Model simulation
-            optimized_mode = model_template(
+            model = model_template(
                 state_variables=state_variables,
-                parameters=init_params,
+                parameters=parameters,
                 t_span=range_days,
                 t_steps=days
             )
 
-            results = optimized_mode.run_model(method='RK45')
+            start_time = time.time()
+            results = model.run_model(method='RK45')
+            if not results.success:
+                raise RuntimeError('Can not resolve simulation')
 
             df_results = cls.result_to_dataframe(
                 results,
                 simulation,
                 state_variables
             )
-            df_results.insert(
-                1,
-                f"{variable_model.name}_reference",
-                reference_values
-            )
+            is_optimized = simulation.parameter_type == ParameterType.OPTIMIZED
+
+            if is_optimized:
+
+                variable_to_fit = next((
+                    variable for variable in simulation.state_variable_limits
+                    if variable.to_fit
+                ), None)
+                assert variable_to_fit, "Undefined"
+
+                variable_model = next((
+                    variable for variable in state_variables
+                    if variable.representation == variable_to_fit.representation
+                ), None)
+                assert variable_model, "Undefined"
+
+                file = cls.get_upload_file(simulation)
+                df_reference = cls.format_reference_dataframe(
+                    file,
+                    variable_model
+                )
+                reference_values = df_reference[
+                    variable_to_fit.representation
+                ].to_numpy()
+
+                algorithm_kwargs = {
+                    'popsize': 20,
+                    'disp': True,
+                    'tol': 0.0015,
+                    'maxiter': 100,
+                    'mutation': [0.3, 0.7],
+                    'atol': 200
+                }
+
+                optimizer = Optimizer(
+                    model=model,
+                    reference_state_variable=variable_model,
+                    reference_values=reference_values,
+                    reference_t_values=results.t
+                )
+
+                optimal = optimizer.optimize(
+                    cost_method='root_mean_square',
+                    algorithm='differential_evolution',
+                    algorithm_kwargs=algorithm_kwargs
+                )
+                optimized_params = optimal.x
+
+                # Update Params
+                init_params = parameters.copy()
+                for index in range(len(parameters)):
+                    init_params[index].initial_value = optimized_params[index]
+
+                # Update Model simulation
+                optimized_mode = model_template(
+                    state_variables=state_variables,
+                    parameters=init_params,
+                    t_span=range_days,
+                    t_steps=days
+                )
+
+                results = optimized_mode.run_model(method='RK45')
+
+                df_results = cls.result_to_dataframe(
+                    results,
+                    simulation,
+                    state_variables
+                )
+                df_results.insert(
+                    1,
+                    f"{variable_model.name}_reference",
+                    reference_values
+                )
+
+        except Exception as error:
+            simulation.update(status=SimulationStatus.ERROR)
+            simulation.reload()
+            SendEmailUseCase.simulation_email(simulation, user, error=True)
+            raise error
 
         end_time = DateTime.format_seconds(time.time() - start_time)
 
@@ -133,10 +154,14 @@ class ExecuteSimulationUseCase:
         )
 
         try:
-            simulation.update(execution_time=end_time)
+            simulation.update(
+                execution_time=end_time,
+                status=SimulationStatus.DONE
+            )
         finally:
             simulation.reload()
 
+        SendEmailUseCase.simulation_email(simulation, user)
         cls.save_figures(fig_results, simulation)
         cls.save_simulation(df_results, simulation)
 
@@ -170,7 +195,7 @@ class ExecuteSimulationUseCase:
         return [
             StateVariable(
                 name=variable.label,
-                representation=variable.label,
+                representation=variable.representation,
                 initial_value=variable.value,
             ) for variable in simulation.state_variable_limits
         ]
@@ -214,7 +239,6 @@ class ExecuteSimulationUseCase:
             file_name = f"{figure.layout.title.text}.html"
             buffer_html = StringIO()
             buffer_json = StringIO()
-            figure.show()
 
             figure.write_html(buffer_html)
             figure.write_json(buffer_json)
